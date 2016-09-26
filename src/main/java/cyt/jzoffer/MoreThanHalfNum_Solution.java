b48cdfbb5c67b53f2538092a5edab421a8438bca
ytchen <120903400@qq.com>
2016-09-27 00:16:47 +0800

    数组中出现次数超过一半的数字

--- src/main/java/cyt/jzoffer/MoreThanHalfNum_Solution.java
+++ src/main/java/cyt/jzoffer/MoreThanHalfNum_Solution.java
@@ -10,6 +10,11 @@ import java.util.Map;
  * @author: ytchen
  * @version:
  * @Date: 2016/9/27
+ * @History: //历史修改记录
+ * <author>  // 修改人
+ * <time> //修改时间
+ * <version> //版本
+ * <desc> // 描述修改内容
  */
 public class MoreThanHalfNum_Solution {
     public static void  main(String[] args){
@@ -19,6 +24,7 @@ public class MoreThanHalfNum_Solution {
     }
 
     /**
+     * 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
      * @param input
      * @param k
      * @return
@@ -27,10 +33,17 @@ public class MoreThanHalfNum_Solution {
 
     }*/
     /**
+     * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+     * 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
      * @param array
      * @return
      */
     /**
+     * 采用阵地攻守的思想：
+     第一个数字作为第一个士兵，守阵地；count = 1；
+     遇到相同元素，count++;
+     遇到不相同元素，即为敌人，同归于尽,count--；当遇到count为0的情况，又以新的i值作为守阵地的士兵，继续下去，到最后还留在阵地上的士兵，有可能是主元素。
+     再加一次循环，记录这个士兵的个数看是否大于数组一般即可。
      * @param array
      * @return
      */
@@ -57,6 +70,7 @@ public class MoreThanHalfNum_Solution {
     }
 
     /**
+     * 最普通的做法
      * @param array
      * @return
      */
